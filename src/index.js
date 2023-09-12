c6f71f8a98c0faf1485c6070505fb5af12cc02ce
Tema444eboi <137617703+Tema444eboi@users.noreply.github.com>
2023-09-12 20:46:37 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+  var result = '';
   
+  while(number>0){
+   result = result + (number%10);
+   number = parseInt(number/10);
+  }
+  
+  return result;
 }
