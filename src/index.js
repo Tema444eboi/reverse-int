75423762ea281df49bf4ca64348941d2e5e18c5c
Tema444eboi <137617703+Tema444eboi@users.noreply.github.com>
2023-09-13 20:02:41 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse(num) {
-  if (num < 0) return -reverse(-num);
+  if (num < 0) return reverse(-num);
   var str = num.toString();
   var strArray = str.split("");
   var revArray = strArray.reverse();
